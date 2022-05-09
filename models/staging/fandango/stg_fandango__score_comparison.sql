d78dbdccb3359c8d7e3442da21e853c4111821e8
David-adejumo <daveadejumo@gmail.com>
2022-05-09 14:58:44 +0100

    adding custom schema for stg

--- models/staging/fandango/stg_fandango__score_comparison.sql
+++ models/staging/fandango/stg_fandango__score_comparison.sql
@@ -28,9 +28,8 @@ final as (
       "METACRITIC_USER_VOTE_COUNT" as metacritic_user_vote_count,
       "IMDB_USER_VOTE_COUNT" as imdb_user_vote_count,
       "FANDANGO_VOTES" as fandango_votes,
-      "FANDANGO_DIFFERENCE" as fandango_difference,
-        "FANDANGO_DIFFERENCE" as fandango_test 
-    
+      "FANDANGO_DIFFERENCE" as fandango_difference
+      
     
 
   from score_comparison
